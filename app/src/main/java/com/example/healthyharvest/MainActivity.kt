3277b0b8092f2b8fd0fc1f4c74dd6bbe666bb46f
AjayRahul1 <p.ajayrahul02@gmail.com>
2022-12-22 11:16:59 +0530

    Re-Design of Healthy Harvest

--- app/src/main/java/com/example/healthyharvest/MainActivity.kt
+++ app/src/main/java/com/example/healthyharvest/MainActivity.kt
@@ -2,10 +2,10 @@ package com.example.healthyharvest
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.RadioButton
-import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,17 +15,11 @@ class MainActivity : AppCompatActivity() {
         // Hide action bar
         supportActionBar?.hide()
 
-        Toast.makeText(applicationContext, "Harvesting Welcomes you ✨", Toast.LENGTH_SHORT).show()
+        Snackbar.make(findViewById(R.id.main_home), "Harvesting Welcomes you ✨", Toast.LENGTH_SHORT).show()
 
-        //radio button transfers to soil selection
-        val chooseAgricultureButton = findViewById<RadioGroup>(R.id.radio_buttons_choose_agriculture_as)
-        chooseAgricultureButton.setOnCheckedChangeListener { radioGroup, i ->
-            val professionChosen = findViewById<RadioButton>(i)
-            if(professionChosen!=null){
-                val soilSelectionIntent = Intent (this, SeasonSelection::class.java)
-                startActivity(soilSelectionIntent)
-                radioGroup.clearCheck()
-            }
+        val fabNextToSeason = findViewById<FloatingActionButton>(R.id.fab_next_to_season)
+        fabNextToSeason.setOnClickListener {
+            startActivity(Intent(this,SeasonSelection::class.java))
         }
     }
 }
--- app/src/main/java/com/example/healthyharvest/SeasonSelection.kt
+++ app/src/main/java/com/example/healthyharvest/SeasonSelection.kt
@@ -2,6 +2,7 @@ package com.example.healthyharvest
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
@@ -15,19 +16,7 @@ class SeasonSelection : AppCompatActivity() {
         // Action bar title changed to Getting started
         supportActionBar?.title = "Getting started!"
 
-        val funFactsArray = arrayOf<String>(
-            "More than 6️⃣0️⃣0️⃣0️⃣ varieties of 🍎 are grown around the world.",
-            "🍌 are the number 1️⃣ fruit crop in the world 👑",
-            "🐝 contribute to more than \$15 billion worth of crops every year through pollination.",
-            "Technically 🍅 is a fruit.", "Not all 🍊 are 🟠 in colour.",
-            "A pomegranate can hold up to 1️⃣0️⃣0️⃣0️⃣ seeds.",
-            "Yams and sweet potatoes are not the same thing!",
-            "🎃 and 🥒 are also classed as a fruit, not a vegetable."
-        )
-
-        // Generating Random facts on the season screen
-        val randomFactsText = findViewById<TextView>(R.id.random_facts)
-        randomFactsText.text = funFactsArray.random()
+        generateRandomFact()
 
         val seasonRadioGrp = findViewById<RadioGroup>(R.id.seasonRadioGrp)
         seasonRadioGrp.setOnCheckedChangeListener { radioGroup, j ->
@@ -46,5 +35,21 @@ class SeasonSelection : AppCompatActivity() {
                 startActivity(soilSelectionIntent) // data sent with going to next screen
             }
         }
+    }
+    private fun generateRandomFact(){
+        val funFactsArray = arrayOf(
+            "More than 6️⃣0️⃣0️⃣0️⃣ varieties of 🍎 are grown around the world.",
+            "🍌 are the number 1️⃣ fruit crop in the world 👑",
+            "🐝 contribute to more than \$15 billion worth of crops every year through pollination.",
+            "Technically 🍅 is a fruit.", "Not all 🍊 are 🟠 in colour.",
+            "A pomegranate can hold up to 1️⃣0️⃣0️⃣0️⃣ seeds.",
+            "Yams and sweet potatoes are not the same thing!",
+            "🎃 and 🥒 are also classed as a fruit, not a vegetable."
+        )
+
+        // Generating Random facts on the season screen
+        val randomFactsText = findViewById<TextView>(R.id.random_facts)
+        randomFactsText.text = funFactsArray.random()
+        Log.d("Random Fact", funFactsArray.random())
     }
 }
